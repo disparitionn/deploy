e21f3fbbaf36d814711b49c17f3c7ea4787f681f
disparitionn <disparitionn@gmail.com>
2019-12-19 18:15:24 +0300

    commit

--- index.js
+++ index.js
@@ -11,26 +11,13 @@ const defaultConfig = config.defaultConfig;
 const filesOrder = config.filesOrder;
 
 let files = [];
-let lowPriorityFiles = [];
+let indexedInfo = {};
 
-
-const filePriority = (file) => {
-    lowPriorityFiles = JSON.parse(filesOrder);
-    console.log(lowPriorityFiles)
-
-    /*if (filesOrder.find(file)){
-        lowPriorityFiles.push(file)
-    }*/
-
-    return lowPriorityFiles;
-};
-
-let bypass = function (dir, done, rootPath) {
-    filePriority()
+const bypass = (dir, done, rootPath) => {
     let results = [];
     rootPath = rootPath || `${__dirname}/${dir}/`;
     rootPath = rootPath.replace(/\/deploy/g, '');
-    fs.readdir(dir, function (err, list) {
+    fs.readdir(dir,  (err, list) => {
         if (err) {
             return done(err);
         }
@@ -38,11 +25,11 @@ let bypass = function (dir, done, rootPath) {
         if (!pending) {
             return done(null, results);
         }
-        list.forEach(function (file) {
+        list.forEach((file) => {
             file = path.resolve(dir, file);
-            fs.stat(file, function (err, stat) {
+            fs.stat(file, (err, stat) => {
                 if (stat && stat.isDirectory()) {
-                    bypass(file, function (err, res) {
+                    bypass(file, (err, res) => {
                         results = results.concat(res);
                         if (!--pending) done(null, results);
                     }, rootPath);
@@ -57,42 +44,14 @@ let bypass = function (dir, done, rootPath) {
     });
 };
 
-bypass(localDir, function (err, results) {
+bypass(localDir,  (err, results) => {
     if (err) throw err;
     files = results;
 });
 
-
-
-const upload = () => {
-    return new Promise(async (resolve, reject) => {
-        let i = files.length - 1;
-        const isFolderCreated = {};
-
-        for (; i >= 0; i--) {
-            try {
-                let locpath = `${localDir}/${files[i]}`;
-                let rempath = `${remoteDir}/${files[i]}`;
-
-                let fileDir = path.dirname(rempath);
-                if (!isFolderCreated[fileDir]) {
-                    const results = await createDirIfNotExist(fileDir);
-                    isFolderCreated[fileDir] = true;
-                    console.log(results);
-                }
-                await sftp.put(locpath, rempath, {flag: 'w'});
-                console.log(files[i], ': UPLOADED');
-            } catch (e) {
-                console.log(files[i], ': ERROR', e)
-            }
-        }
-        resolve()
-    })
-};
-
-function createDirIfNotExist(path) {
-    return new Promise((resolve, reject)=>{
-        sftp.exists(path + '/').then(result =>{
+const createDirIfNotExist = (path) => {
+    return new Promise((resolve, reject) => {
+        sftp.exists(path + '/').then(result => {
             if (!!result) {
                 return resolve(`${path} exists`)
             }
@@ -101,7 +60,56 @@ function createDirIfNotExist(path) {
                 .catch(reject)
         }).catch(reject)
     });
+};
+
+const isLowPriority = (filesOrder) => {
+    const result = {};
+    Object.values(filesOrder.files).forEach((arr)=>{
+        arr.forEach((path)=>{
+            result[path] = true;
+        })
+    })
+};
+//todo
+const indexingFiles = () => {
+    return;
 }
+
+const filePriority = () => {
+    return;
+};
+
+const upload = () => {
+    indexingFiles();
+    return new Promise(async (resolve, reject) => {
+        let i = files.length - 1;
+        const isFolderCreated = {};
+
+        for (; i >= 0; i--) {
+            if (!isLowPriority(files[i], indexedInfo)) {
+                continue;
+            }
+            try {
+                let localPath = `${localDir}/${files[i]}`;
+                let remotePath = `${remoteDir}/${files[i]}`;
+
+                let fileDir = path.dirname(remotePath);
+                if (!isFolderCreated[fileDir]) {
+                    const results = await createDirIfNotExist(fileDir);
+                    isFolderCreated[fileDir] = true;
+                    console.log(results);
+                }
+                await sftp.put(localPath, remotePath, {flag: 'w'});
+                console.log(files[i], ': UPLOADED');
+            } catch (e) {
+                console.log(files[i], ': ERROR', e)
+            }
+        }
+
+        resolve()
+    })
+};
+
 
 const sftp = new Client();
 (async () => {
@@ -109,7 +117,7 @@ const sftp = new Client();
         await sftp.connect(defaultConfig);
         const p = await sftp.cwd();
         console.log(`Remote working directory is ${p}`);
-        //await upload();
+        await upload();
         await sftp.end();
         console.log('------------Deploy was completed------------');
     } catch (e) {
